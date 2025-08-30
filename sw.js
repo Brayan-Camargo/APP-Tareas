827155a1f7aac1118ac2185b8bb6858c5873dafd
Brayan-Camargo <brayancamargoramirez@gmail.com>
2025-08-30 17:42:55 -0600

    se agregaron los archivos json y uno adicioona de js para la instalacion y finalmente un pequeño script en el header de html para vincular los archivos antes mencionados

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'tarea-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Le dice al navegador que guarde en caché los archivos
